a367956c8509de06a734af9bcb8a47d1da29f72b
megatron-github <62828416+megatron-github@users.noreply.github.com>
2020-10-31 01:47:36 -0400

    Add files via upload

---
+++ windchill.py
@@ -0,0 +1,35 @@
+"""
+ *****************************************************************************
+   FILE:        windchill.py
+
+   AUTHOR:		Truong Pham
+
+   ASSIGNMENT: 	Project_Winchill
+
+   DATE:		8/28/18
+
+   DESCRIPTION:	Create a windchill calculator
+
+ *****************************************************************************
+"""
+
+def windchill_calculator():
+    """ Windchill calculator starts here """
+    print('Welcome to the windchill calculator!')
+
+    temper = float(input('Enter the temperature: '))
+    windspeed = float(input('Enter the wind speed: '))
+    #input() both print and scan in python
+
+    #5^6 = 5**6 in python
+    windchill = float(35.74 + 0.6215 * temper - 35.75 * windspeed**0.16 + 
+                      0.4275 * temper * windspeed**0.16)
+    #Source: How Is Wind Chill Calculated? | Mental Floss
+    #Cite: http://mentalfloss.com/article/26730/how-wind-chill-calculated
+
+    print("At", temper, "degrees, with a wind speed of", windspeed, "miles" +
+          " per hour,")
+    print("the windchill is:", windchill, "degrees")
+
+if __name__ == "__main__":
+    windchill_calculator()
